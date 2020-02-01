85625f4b318bc7fa6abe5f64a706d1499ddbcfa3
Patryk <patryk.iwasieczko@gmail.com>
2020-02-01 20:15:00 +0100

    Set up app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
+// React
 import React from "react";
 import "./App.scss";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Apollo
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import logo from "./logo.png";
+
+const client = new ApolloClient({
+    uri: "http://localhost:5000/graphql"
+});
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello</h1>
-        </div>
+        <ApolloProvider client={client}>
+            <BrowserRouter>
+                <div className="container">
+                    <img className="logo" src={logo} alt="SpaceX" />
+                </div>
+            </BrowserRouter>
+        </ApolloProvider>
     );
 }
 
